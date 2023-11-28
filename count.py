59db31b3002229c7d57ef5bc91f3a60b6846da9e
Min KyungHyun <khmin1104@gmail.com>
2023-11-28 17:58:34 +0900

    feat: add tool

--- count.py
+++ count.py
@@ -25,27 +25,30 @@ SEARCH_FIELDS = [
     'year'
 ]
 
+# if __name__ == "__main__":
 with open("./ACL_PAPERS.json",'r') as f:
     data = json.load(f)
 
 data = filter_non_abstract(data)
 abstract = [paper.abstract for paper in data]
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-vectorizer = TfidfVectorizer()
+from sklearn.feature_extraction.text import CountVectorizer
+
+vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(abstract)
+
 # vectorizer.get_feature_names_out()
 print(X.shape)
 print(vectorizer.vocabulary_) # bog of word
 print(X)
 print(len(vectorizer.get_feature_names_out()))
-print(vectorizer.idf_)
 
 for i, paper in enumerate(data):
     if "Knowledge Graphs" in paper.title:
         print(i, paper.title)
 
 p = abstract[293]
+print(p)
 response = vectorizer.transform([p])
 p_tdfidf = [(col, vectorizer.get_feature_names_out()[col], response[0, col]) for col in response.nonzero()[1]] 
 p_tdfidf.sort(key=lambda x: x[2], reverse=True)
--- tool.py
+++ tool.py
@@ -1,3 +1,4 @@
+import re
 class dotdict(dict):
     """dot.notation access to dictionary attributes"""
     __getattr__ = dict.get
@@ -5,4 +6,26 @@ class dotdict(dict):
     __delattr__ = dict.__delitem__
 
 
-## re.sub(r'https?://\S+', '', text)
+def filter_non_abstract(data):
+    data = list(map(dotdict, data))
+    abstract_len = {}
+    no_abstract = []
+    for i, paper in enumerate(data):
+        if paper.abstract:
+            abstract_len[i] = len(paper.abstract)
+        else:
+            no_abstract.append(i)
+
+    print("len of no_abstract:",len(no_abstract))
+
+    abstract_len = sorted(abstract_len.items(), key=lambda x:x[1])
+
+    defect_abstract_idx = [ i for i,n in abstract_len if n < 100 ] + no_abstract
+    print("len of defect_abstract_idx:",len(defect_abstract_idx))
+
+    for i in sorted(defect_abstract_idx, reverse=True):
+        del data[i]
+    return data
+
+def remove_url(text):
+    return re.sub(r'https?://\S+', '', text)
