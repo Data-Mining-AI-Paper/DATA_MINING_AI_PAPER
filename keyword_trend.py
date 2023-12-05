63d4011143220670ecfecefe36ebd684bf00c19d
JongHoB <78012131+JongHoB@users.noreply.github.com>
2023-12-05 18:38:15 +0900

    [Modify] tf-idf avg

--- keyword_trend.py
+++ keyword_trend.py
@@ -2,6 +2,7 @@ import pickle
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from collections import defaultdict
+from sklearn.preprocessing import MinMaxScaler, StandardScaler,RobustScaler
 
 from tool import *
 from tf_idf import MyTfidfVectorizer
@@ -46,9 +47,15 @@ num_keywords = [len(important_words_by_year[year].keys()) for year in years]
 num_research_topic = num_keywords
 weight_research_topic = np.array(num_research_topic) / np.sum(num_research_topic) 
 for i, year in enumerate(data_by_year.keys()):
-    for word, tfidf in important_words_by_year[year].items():
-        important_words_by_year[year][word] = tfidf / len(data_by_year[year]) * weight_research_topic[i]
+    
+    # Normalize important_words_by_year[year] values with Scaler
 
+    scaler = RobustScaler()
+    temp=scaler.fit_transform(np.array(list(important_words_by_year[year].values())).reshape(-1,1))
+    important_words_by_year[year]=dict(zip(important_words_by_year[year].keys(),temp.reshape(-1)))
+    
+    for word, tfidf in important_words_by_year[year].items():
+        important_words_by_year[year][word] = tfidf / len(data_by_year[year])# * weight_research_topic[i]
 
 from random import sample
 keyword_to_visual = []
@@ -95,13 +102,19 @@ keyword_trends = []
 for idx, keyword in enumerate(keyword_to_visual):
     keyword_trend = []
     for year in years:
-        if keyword in important_words_by_year[year]:
+        if keyword in important_words_by_year[year] and important_words_by_year[year][keyword] > 0:
             keyword_trend.append(important_words_by_year[year][keyword])
+            
         else:
-            keyword_trend.append(0)
+            if year != 1979 and year != 2023:
+                prev_year = important_words_by_year[year-1][keyword] if keyword in important_words_by_year[year-1] else 0
+                next_year = important_words_by_year[year+1][keyword] if keyword in important_words_by_year[year+1] else 0
+                keyword_trend.append((prev_year + next_year)/2)
+            else:
+                keyword_trend.append(0)
+
     keyword_trends.append(keyword_trend)
     
-
 plt.figure(figsize=(10, 6))
 for i in range(len(keyword_to_visual)):
     plt.plot(years, keyword_trends[i], marker='o', linestyle='-')
